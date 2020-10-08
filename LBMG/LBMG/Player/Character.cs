70fa1d93076494d7450da3b47684b07db803f84d
Ilian BAYLON <ilian.baylon@epitech.eu>
2020-10-08 15:08:01 +0200

    [+] Character coordinates

--- LBMG/LBMG/Player/Character.cs
+++ LBMG/LBMG/Player/Character.cs
@@ -12,12 +12,45 @@ namespace LBMG.Player
         public float Speed { get; set; }
         public Direction Direction { get; set; }
         public bool IsMoving { get; set; }
+        public Point Position { get; set; }
         public Character(string name, float speed)
         {
             Name = name;
             Speed = speed;
             Direction = Direction.Bottom;
             IsMoving = false;
+            Position = new Point(0, 0);
+        }
+
+        public void SpawnAt(int x, int y/*, Map map*/)
+        {
+            Position = new Point(x, y);
+        }
+
+        public void Move()
+        {
+            int x = Position.X;
+            int y = Position.Y;
+
+            x += Direction switch
+            {
+                Direction.Left => -1,
+                Direction.Top => 0,
+                Direction.Right => 1,
+                Direction.Bottom => 0,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            y += Direction switch
+            {
+                Direction.Left => 0,
+                Direction.Top => 1,
+                Direction.Right => 0,
+                Direction.Bottom => -1,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            Position = new Point(x, y);
         }
     }
 
