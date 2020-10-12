82f465c9c2da654ed50ba0645c52fba476ffbbb9
Ilian BAYLON <ilian.baylon@epitech.eu>
2020-10-12 10:21:59 +0200

    [+] UI class with its DialogBox and its methods like Write or NextDialog && TextBank class with all the game texts + language system

--- LBMG/LBMG/UI/DialogBox.cs
+++ LBMG/LBMG/UI/DialogBox.cs
@@ -1,10 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace LBMG.UI
 {
-    class DialogBox
+    public class DialogBox
     {
+        private bool Visible { get; set; }
+
+        public List<string> TextWritten { get; set; }
+
+        public int CurrentTextIndex = 0;
+
+        public Point Size { get; set; }
+
+        //public Font Font { get; set; }
+
+        public int CharWidth { get; set; }
+
+        public int DisplaySpeed { get; set; }
+
+        public DialogBox()
+        {
+            Visible = false;
+        }
+
+        public void Write(List<int> keysOfTextToDisplay)
+        {
+            if (Visible)
+                return;
+
+            TextWritten = new List<string>();
+            Visible = true;
+
+            foreach (var key in keysOfTextToDisplay)
+            {
+                List<string> texts = WrapText(TextBank.GetText(key));
+
+                foreach (var text in texts)
+                {
+                    TextWritten.Add(text);
+                }
+            }
+            Debug.WriteLine(TextWritten[CurrentTextIndex]);
+        }
+
+        public void Write(int keyOfTextToDisplay)
+        {
+            if (Visible)
+                return;
+
+            TextWritten = new List<string>();
+            Visible = true;
+
+            List<string> texts = WrapText(TextBank.GetText(keyOfTextToDisplay));
+
+            foreach (var text in texts)
+            {
+                TextWritten.Add(text);
+            }
+            Debug.WriteLine(TextWritten[CurrentTextIndex]);
+        }
+
+        public void NextDialog()
+        {
+            if (Visible == false)
+                return;
+
+            if (CurrentTextIndex == TextWritten.Count - 1)
+            {
+                EndDialog();
+                return;
+            }
+
+            CurrentTextIndex++;
+            Debug.WriteLine(TextWritten[CurrentTextIndex]);
+        }
+
+        public void EndDialog()
+        {
+            CurrentTextIndex = 0;
+            Visible = false;
+            TextWritten.Clear();
+            Debug.WriteLine("Dialog end");
+        }
+
+        private List<string> WrapText(string textToWrap)
+        {
+            var str = textToWrap.Split(' ').ToList();   // TODO Add some wrapping logic
+            return str;
+        }
     }
 }
